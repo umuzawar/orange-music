c4a5d0bc95a7cd3f28f62fa181872c1c339f954b
umuzawar <121365873+umuzawar@users.noreply.github.com>
2022-12-25 14:46:59 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("C5 - A - B A G - ", 159)
+    music.playMelody("C5 A G B - A G - ", 159)
+    music.playMelody("C5 A G A B A G - ", 159)
+    music.playMelody("C5 A G B B A G C5 ", 159)
+    music.playMelody("C5 - C5 - C5 B C5 - ", 159)
+})
